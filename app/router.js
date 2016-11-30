21bd85cbf831b6c90c316f19989e8be9689e5627
rusanu <contact@rusanu.com>
2016-11-30 20:44:24 +0200

    add product route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('products', function() {
+    this.route('add');
+  });
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('product');
+  },
+
+  actions: {
+    addProduct() {
+      this.transitionTo('products.add');
+    }
   }
 });
